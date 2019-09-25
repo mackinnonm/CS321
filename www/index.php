edb664cacf2926a1f8cb596129e39b98fb25cdec
Michele MacKinnon <49697668+mackinnonm@users.noreply.github.com>
2019-09-25 12:15:24 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,3 +1,67 @@
-this is a test
+<!doctype html>
+<html lang="en">
+  <head>
+    <!--author:     Michele MacKinnon
+        assignment: CS321 Assignment 2
+        date:       22sept2019-->
+
+   <!-- <link href="styles.css" type="text/css" rel="stylesheet" />-->
+            
+    <meta charset="utf-8">
+    <title>Isle of Skye, Scotland</title>
+    <style type="text/css">
+    </style>
+  </head>
+  <body>
+    
+      <header>
+              <ul>
+                <li><a>HOME</a></li>
+                <li><a>LODGING</a></li>
+                <li><a>RESTAURANTS</a></li>
+                <li><a>TRAVEL</a>
+                    <ul>
+                      <li><a>Ferry</a></li>
+                      <li><a>Car Rental</a></li>
+                    </ul>
+                <li><a>ATTRACTIONS</a></li>
+                <li><a>ABOUT US</a<>
+                    <ul>
+                      <li><a>FAQs<a<></li>
+                      <li><a>Contact Us<a<></li>
+                      <li><a>Advertise<a<></li>
+                    </ul>    
+                </li>
+
+          <header>
+            <marquee><h1>IsleofSkyeScotland.com</h1></marquee>
+            
+            <form>
+            <input type = "text" name = "text" class = "search" placeholder = "search here">
+            <input type = "submit" name = "submit" class = "submit" value="Search"> 
+            </form> 
+           
+  
+  <div class="content">
+    <h1><center>The Connection Between the MacKinnon clan, Skye and Drambuie<center></h1>
+      
+      <article>
+              The intent of this website is to offer a creative and informative website about the Isle of Skye which not only inspires travel to Skye but promotes a genuine passion and appreciation for the beauty and heritage of all things Skye and MacKinnon related.
+     </article>
+                    <p>The <a href="https://www.isleofskye.com/">
+Isle of Skye<a/>, fondly referred to as Skye, is a small island off the West Coast of Scotland's Highlands in the United Kingdom of Great Britain.  Skye is one of the top locations in Scotland to visit. It is famous for its scenery and landscapes that will take your breath away.
+                    </p>
+                    <p>The <a href="https://www.scotweb.co.uk/info/mackinnon/"> MacKinnon</a> clan can date it's history on the Isle of Skye from the close of the 14th century until the 18th century.                     </p>
+                    <p>
+                      <a href="https://www.drambuie.com/"> Drambuie</a> history and connection with the MacKinnon clan and Skye...
+                    </p>
+      </div>
+        <footer>
+         <center> Home|Privacy|Contact <br><center>
+          <center>&copy 2019|isleofskyescotland.com|All Rights Reserved<center>
+        </footer>  
+   </body>
+</html>       
+   
 
 
