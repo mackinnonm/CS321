85af1a90754fe96a9252b05df7c154d59f3d5484
Michele <mackinnm.com>
2019-10-09 20:35:01 -0400

    assignment 3

---
+++ www/contact.php
@@ -0,0 +1,49 @@
+<!doctype>
+
+<!--author:     Michele MacKinnon
+    assignment: CS333 -->
+  <html>
+    <head> 
+    <meta charset="UTF-8">
+      <link rel="stylesheet" href="css/cs321_contact2_css.css" />      
+     
+      
+    </head>  
+    
+    <body>
+      <div id="glob_content">
+        
+        <div id="title">Contact Us</div>
+        
+        <div id="form_content">
+          
+          <form>
+            
+            <input type="text" name="name" placeholder="Name"/><br>
+            <input type="email" name="email" placeholder="Email"/><br>
+            <input type="text" name="phone" placeholder="Phone Number"/><br>
+       <h2>How helpful was this website?</h2>
+        <input type="checkbox" name="useful1" value="vuseful">Very Helpful<br>
+      <input type="checkbox" name="useful2" value="someuseful">Somewhat Helpful<br>
+      <input type="checkbox" name="useful3" value="notuseful">Not Helpful<br>
+            <h2>What is your reason for contacting us?</h2>
+     
+             <select> 
+              <option>To request additional information.</option>
+              <option>To offer positive feedback.</option>
+              <option>To offer constructive criticism.</option>
+            </select> <br>
+            
+                      
+            <input type="submit" name="send" value="Submit"/>
+            
+            
+          
+            
+            
+          </form>
+        
+        </div>
+      </div>
+    </body>   
+</html>    
